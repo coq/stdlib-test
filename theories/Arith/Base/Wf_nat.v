f74c0e32d5abb4f706d1ef86a938e126a3b59287
Pierre Roux <pierre@roux01.fr>
2023-12-17 18:16:01 +0100

    Add Makefile for arith-base

--- theories/Arith/Base/Wf_nat.v
+++ theories/Arith/Base/Wf_nat.v
@@ -242,7 +242,7 @@ Qed.
 Set Implicit Arguments.
 
 From Stdlib Require Import Compare_dec.
-From Stdlib Require Import Decidable.
+From Stdlib.Logic Require Import Decidable.
 
 Definition has_unique_least_element (A:Type) (R:A->A->Prop) (P:A->Prop) :=
   exists! x, P x /\ forall x', P x' -> R x x'.
