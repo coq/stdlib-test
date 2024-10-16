af0b224bcee2594f7aa9f7575638cb2267206210
Pierre Roux <pierre.roux@onera.fr>
2024-10-16 09:49:14 +0200

    Adapt changelog

--- dev/tools/make-changelog.sh
+++ dev/tools/make-changelog.sh
@@ -3,7 +3,7 @@
 printf "PR number? "
 read -r PR
 
-printf "Category? (type a prefix)\n"
+printf "Category? (type a prefix, launch script multiple times if multiple categories)\n"
 (cd doc/changelog && ls -d */)
 read -r where
 
@@ -11,31 +11,20 @@ where="doc/changelog/$where"
 if ! [ -d "$where" ]; then where=$(echo "$where"*); fi
 where="$where/$PR-$(git rev-parse --abbrev-ref HEAD | tr / -).rst"
 
-printf "Type? (type first letter)\n"
-printf "[A]dded \t[C]hanged \t[D]eprecated \t[F]ixed \t[R]emoved\n"
-read -r type_first_letter
-
-case "$type_first_letter" in
-  [Aa]) type_full="Added";;
-  [Cc]) type_full="Changed";;
-  [Dd]) type_full="Deprecated";;
-  [Ff]) type_full="Fixed";;
-  [Rr]) type_full="Removed";;
-  *) printf "Invalid input!\n"
-     exit 1;;
-esac
+printf "File?\n"
+read -r in_file
 
 printf "Fixes? (space separated list of bug numbers)\n"
 read -r fixes_list
 
-fixes_string="$(echo $fixes_list | sed 's/ /~  and /g; s,\([0-9][0-9]*\),`#\1 <https://github.com/coq/coq/issues/\1>`_,g' | tr '~' '\n')"
-if [ ! -z "$fixes_string" ]; then fixes_string="$(printf '\n  fixes %s,' "$fixes_string")"; fi
+fixes_string="$(echo $fixes_list | sed 's/ /~  and /g; s,\([0-9][0-9]*\),`#\1 <https://github.com/coq/stdlib/issues/\1>`_,g' | tr '~' '\n')"
+if [ ! -z "$fixes_string" ]; then fixes_string="$(printf '\n    fixes %s,' "$fixes_string")"; fi
 
 # shellcheck disable=SC2016
 # the ` are regular strings, this is intended
 # use %s for the leading - to avoid looking like an option (not sure
 # if necessary but doesn't hurt)
-printf '%s **%s:**\n  Describe your change here but do not end with a period\n  (`#%s <https://github.com/coq/coq/pull/%s>`_,%s\n  by %s).\n' - "$type_full" "$PR" "$PR" "$fixes_string" "$(git config user.name)" > "$where"
+printf '%s in `%s`\n\n  + Describe your change here but do not end with a period\n    for instance: lemmas `lem1`, `lem2` and `lem3`\n    (`#%s <https://github.com/coq/stdlib/pull/%s>`_,%s\n    by %s).\n' - "$in_file" "$PR" "$PR" "$fixes_string" "$(git config user.name)" > "$where"
 
 printf 'Name of created changelog file:\n'
 printf '%s\n' "$where"
