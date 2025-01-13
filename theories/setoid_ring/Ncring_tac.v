coq/coq

split Zdivisibility from Znumtheory, deprecate most of Znumtheory

theories/setoid_ring/Ncring_tac.v
@@ -12,9 +12,8 @@ Require Import List.
 Require Import Setoid.
 Require Import BinPos.
 Require Import BinList.
-Require Import Znumtheory.
 Require Export Morphisms Setoid Bool.
-Require Import ZArith.
+Require Import ZArith_base.
 Require Import Algebra_syntax.
 Require Export Ncring.
 Require Import Ncring_polynom.

coq/stdlib-test

theories/setoid_ring/Ncring_tac.v
@@ -12,9 +12,8 @@ Require Import List.
 Require Import Setoid.
 Require Import BinPos.
 Require Import BinList.
-Require Import Znumtheory.
 Require Export Morphisms Setoid Bool.
-Require Import ZArith.
+Require Import BinInt.
 Require Import Algebra_syntax.
 Require Export Ncring.
 Require Import Ncring_polynom.